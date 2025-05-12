587893a083ba9b74dd14741ff5fa59a9f910952c
Kobadai722 <omoidetabi@gmail.com>
2025-05-12 14:16:30 +0900

    Ver 1.0

---
+++ customer.php
@@ -0,0 +1,12 @@
+<!--!+tab-->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>顧客管理</title>
+</head>
+<body>
+    
+</body>
+</html>
