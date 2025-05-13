d5f79a31796aac821e7ece36029146d892be3876
main <omoidetabi@gmail.com>
2025-05-13 19:42:01 +0900

    fix main.php

--- main.php
+++ main.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="index.css" type="text/css">
-    </head>
-    <body>
-        <div class="header">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css" type="text/css">
+</head>
+
+<body>
+    <div class="header">
         <!-- <a href="main.php"><img src="./amason_img/amason.jpg" class="amasonLogo"></a> -->
         <ul>
             <li>
                 <a href="./logout.php">ログアウト</a>
             </li>
         </ul>
-        </div>
+    </div>
 
-        <h2>ようこそ
-            <?php
+    <h2>ようこそ
+        <?php
                 session_start();
                 echo $_SESSION['dname'];
-            ?>
-        さん</h2>
-        <p> <a href="./../accounting/siwake_hyo/siwakehyo_output.html">仕訳機能プロトタイプ</p>
+            ?> さん</h2>
+    <p> <a href="./../accounting/siwake_hyo/siwakehyo_output.html">仕訳機能プロトタイプ</p>
