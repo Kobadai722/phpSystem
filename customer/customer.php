90268436c6269e72e5880f494d94b97a7b6ea858
Kobadai722 <omoidetabi@gmail.com>
2025-05-13 14:06:58 +0900

    Update Ver2.0

--- customer/customer.php
+++ customer/customer.php
@@ -1,4 +1,5 @@
 <!--!+tab-->
+<?php session_start(); ?> 
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -9,5 +10,20 @@
 <body>
     <h1>顧客管理</h1>
     <h2>以下に一覧が表示される予定</h2>
+    <table  border="1">
+    <tr><th>商品番号</th><th>商品名</th><th>価格</th></tr>
+    <?php
+        $PDO = new PDO('mysql:host=chandou.ltt.jp; dbname=utiraku0428; charset=utf8', 'utiraku', '4sp3Yukt');
+        foreach($pdo->query('select * from CUSTOMER') as $row){
+    ?>
+        <tr>
+        <td><?=$row['CUSTOMER_ID']?></td>
+        <td><a href="detail.php?id=<?=$row['p_id']?>"><?=$row['p_name']?></a></td>
+        <td><?=$row['p_price']?></td>
+        </tr>
+    <?php
+        }
+    ?>
+    </table>
 </body>
 </html>
