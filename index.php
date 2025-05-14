afff9588a33a730c7d90f9a84ec6f0221a3ae923
kobadai722 <omoidetabi@gmail.com>
2025-05-14 11:06:49 +0900

    update from style Ver3.1

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     <div class="container d-flex justify-content-center align-items-center vh-100">
         <div class="row w-100">
             <div class="col-md-6 mx-auto">
-                <form class="form-signin text-center">
+                <form class="form-signin text-center" action="login.php" method="post">
                     <img class="mb-4 mx-auto d-block" src="/images/logo-type2.png" alt="" width="300" height="300">
                     <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
                     <label for="inputCode" class="sr-only">社員コード</label>
@@ -51,6 +51,22 @@
             </div>
         </div>
     </div>
+
+    <form class="form-signin">
+        <img class="mb-4" src=/images/logo-type2.png" alt="" width="300" height="300">
+        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+        <div class="checkbox mb-3">
+            <label>
+            <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-muted">� 2017-2018</p>
+    </form>
 
 </body>
 
