52b61c85cdead63a22822a8a9e7228c971fc7581
main <omoidetabi@gmail.com>
2025-05-20 19:33:46 +0900

    fix index,html bug

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <title>サインインページ</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
