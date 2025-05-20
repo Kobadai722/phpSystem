af2d29e1b3d8d81889cacc207fc541748c54d5ea
Kobadai722 <omoidetabi@gmail.com>
2025-05-20 14:05:34 +0900

    fix table design

--- customer/customer.php
+++ customer/customer.php
@@ -9,17 +9,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
-<?php include 'header.php'; ?>
+<?php include '../header.php'; ?>
 <body>
     <h1>顧客管理</h1>
-    <table class="table table-dark table-hover">
+    <table class="table table-hover">
     <tr><th scope="col">顧客ID</th><th scope="col">氏名</th><th scope="col">電話番号</th><th scope="col">メールアドレス</th><th scope="col">郵便番号</th><th scope="col">住所</th></tr>
     <?php
         require_once '../config.php';
         foreach($PDO->query('select * from CUSTOMER') as $row){
     ?>
         <tr>
-        <td><?=$row['CUSTOMER_ID']?></td>
+        <td scope="row"><?=$row['CUSTOMER_ID']?></td>
         <td><?=$row['NAME']?></td>
         <td><?=$row['CELL_NUMBER']?></td>
         <td><?=$row['MAIL']?></td>
