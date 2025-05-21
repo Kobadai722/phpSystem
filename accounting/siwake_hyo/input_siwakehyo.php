aaf87316fa3dd011f176c1b83e2c725d90ab177f
esrgdtfdrrthnfytdr <20224033-takahashiryo@hcs.ac.jp>
2025-05-21 11:18:27 +0900

    バグ

--- accounting/siwake_hyo/input_siwakehyo.php
+++ accounting/siwake_hyo/input_siwakehyo.php
@@ -1,72 +1,61 @@
 <!DOCTYPE html>
 <html lang="ja">
-
-<head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="siwakehyo.css" type="text/css">
-  <title>仕訳入力</title>
-</head>
-
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="siwakehyo.css" type="text/css">
+    </head>
 <body>
+  <table>
+    <tr>
+      <!-- 仕訳ヘッダー-->
+      <td>日付</td>
+      <td>摘要</td>
+      <!-- 仕訳明細-->
+      <td>貸方科目</td>
+      <td>貸方金額</td>
+      <td>借方科目</td>
+      <td>借方金額</td>
+    </tr>
+    <!-- 仕訳明細 -->
+    <!--借方科目-->
+    <tr>
+      <td><input type="date" name="entry_date"></td><!-- 日付 -->
+
+      <td><input type="text" name="description"></td> <!-- 摘要 -->
+
+      <td>
+        <select name="勘定科目" name="debit_account"> <!-- 借方科目 -->
+          <option value="">売掛金</option>
+      </td>
+
+      <td><input type="number" name="debit_amount"></td>  <!-- 借方金額 -->
+
+      <!--貸方科目-->
+      <td><input type="date" name="entry_date"></td><!-- 日付 -->
+
+      <td><input type="text" name="description"></td> <!-- 摘要 -->
+
+      <td><select name="勘定科目" name="credit_account"><!-- 貸方科目 -->
+            <option value="">売掛金</option></td>
+
+      <td><input type="number" name="credit_amount"></td>  <!-- 貸方金額 -->
+    </tr>
+  </table>
+
+  <!-- 仕訳の登録-->
   <?php
-  session_start();
-  // DB接続
-  require_once '../../config.php';
-
-
-  ?>
-  <!-- 入力フォーム-->
-  <form action="submit_siwake.php" method="post">
-    <table>
-      <tr>
-        <!-- 仕訳ヘッダー-->
-        <td>日付</td>
-        <td>摘要</td>
-        <!-- 仕訳明細-->
-        <td>借方科目</td>
-        <td>借方金額</td>
-        <td>貸方科目</td>
-        <td>貸方金額</td>
-      </tr>
-      <tr>
-        <td><input type="date" name="entry_date"></td><!-- 日付 -->
-
-        <td><input type="text" name="description"></td> <!-- 摘要 -->
-
-        <td>
-          <select name="勘定科目"><!-- 借方科目 -->
-            <?php
-            // 勘定科目の取得
-            $sql = $PDO->prepare('SELECT ID, NAME FROM ACCOUNTS');
-            $sql->execute();
-
-            while ($accounts = $sql->fetch(PDO::FETCH_ASSOC)) {
-              echo '<option value="' . $accounts['ID'] . '">' . $accounts['NAME'] . '</option>';
-            }
-            ?>
-          </select>
-        </td>
-
-        <td><input type="number" name="debit_amount"></td> <!-- 借方金額 -->
-
-        <td>
-          <select name="勘定科目"><!-- 貸方科目 -->
-            <?php
-            while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
-              echo '<option value="' . $row['ID'] . '">' . $row['NAME'] . '</option>';
-            }
-            ?>
-          </select>
-        </td>
-        <td><input type="number" name="credit_amount"></td> <!-- 貸方金額 -->
-      </tr>
-    </table>
-    <br>
-    <p><button type="submit">確定</button></p>
-  </form>
-  <p><a href="../../main.php">トップページに戻る</a></p>
-  <p><a href="input_siwakehyo.php">仕訳入力画面に戻る</a></p>
-  <p><a href="../../index.php">ログアウト</a></p>
-</body>
-
-</html>
+    session_start();
+    // DB接続
+    require_once 'config.php';
+    //仕訳ヘッダーの登録
+    $sql = $PDO->prepare('INSERT INTO JOURNAL_HEADERS (ENTRY_DATE, DESCRIPTION) VALUES(?, ?)');
+    $sql->execute([$_POST['entry_date'], $_POST['description']]);
+    //仕訳明細の登録
+    //ヘッダーIDの取得
+    $sql = $PDO->prepare('SELECT ID FROM JOURNAL_HEADERS WHERE ENTRY_DATE = ? AND DESCRIPTION = ?');
+    $sql->execute([$_POST['entry_date'], $_POST['description']]);
+    $header_id = $pdo->lastInsertId();
+    
+    //借方の登録
+    $sql = $PDO->prepare('INSERT INTO JOURNAL_ENTRY (HEADER_ID, ACCOUNT_ID, AMOUNT, TYPE) VALUES($header_id, ?, 借方)');
+    $sql->execute0([$_POST['debit_account'], $_POST['debit_amount']]);
