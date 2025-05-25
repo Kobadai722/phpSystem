629d09656979f78843f1eeec502d7833899f43fd
esrgdtfdrrthnfytdr <20224033-takahashiryo@hcs.ac.jp>
2025-05-25 22:10:23 +0900

    insert

--- accounting/siwake_hyo/submit_siwake.php
+++ accounting/siwake_hyo/submit_siwake.php
@@ -16,13 +16,9 @@
   //仕訳ヘッダーの登録
   $sql = $PDO->prepare('INSERT INTO JOURNAL_HEADERS (ENTRY_DATE, DESCRIPTION) VALUES(?, ?)');
   $sql->execute([$_POST['entry_date'], $_POST['description']]);
-
   //仕訳明細の登録
-  //ヘッダーIDの取得
-  $sql = $PDO->prepare('SELECT ID FROM JOURNAL_HEADERS WHERE ENTRY_DATE = ? AND DESCRIPTION = ?');
-  $sql->execute([$_POST['entry_date'], $_POST['description']]);
+  // 勘定科目のIDを取得するためのSQL
   $header_id = $PDO->lastInsertId();
-
   //借方の登録
   $sql = $PDO->prepare('INSERT INTO JOURNAL_ENTRY (HEADER_ID, ACCOUNT_ID, AMOUNT, TYPE) VALUES(?, ?, ?, ?)');
   $sql->execute([$header_id, $_POST['debit_account'], $_POST['debit_amount'], '借方']);
