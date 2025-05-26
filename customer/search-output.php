82b806629fb6e74727b6916580c3b9091843a4dd
Kobadai722 <omoidetabi@gmail.com>
2025-05-26 14:58:20 +0900

    add search form

--- customer/search-output.php
+++ customer/search-output.php
@@ -8,7 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container mt-4">
+    <main>
+        <div class="container mt-4">
         <h3>検索結果</h3>
         <table class="table table-hover">
             <tr><th>顧客ID</th><th>氏名</th><th>電話番号</th><th>メールアドレス</th><th>郵便番号</th><th>住所</th></tr>
@@ -38,6 +39,7 @@
             ?>
         </table>
         <a href="customer.php" class="btn btn-secondary">戻る</a>
-    </div>
+        </div>
+    </main>
 </body>
 </html>
