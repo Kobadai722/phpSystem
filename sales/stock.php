912c04d10246460a0dbbb420f0ac2557ac06d95c
tyuando <narinaritosuga@gmail.com>
2025-05-27 14:15:07 +0900

    キューピー

--- sales/stock.php
+++ sales/stock.php
@@ -23,7 +23,7 @@
 
             <section class="content">
                 <section class="search"><!-- コンテンツをグループ化 -->
-                    <div class="input-group mb-3">
+                    <div class="mb-3">
                         <input type="text" class="form-control" placeholder="商品名または商品IDで検索">
                         <button class="btn btn-primary" type="submit">検索</button>
                     </div>
