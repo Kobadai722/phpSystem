7378357b2c2895996e37b5abe301228769b7e8b4
菅原成斗 <20224014-sugawaranarito@hcs.ac.jp>
2025-05-30 12:00:30 +0900

    リストにアイコンんを追加したい

--- sales/stock.php
+++ sales/stock.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"><!-- アイコンのブートストラップ -->
         <title>在庫管理システム</title>
         <link rel="stylesheet" href="stock_styles.css">
         <link rel="stylesheet" href="styles.css">
@@ -14,10 +15,18 @@
         <main>
             <nav class="localNavigation"> 
                 <ul>
-                    <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">在庫管理</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">売上管理</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">発注管理</a></li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="bi bi-house-door-fill"></i> Home</a>
+                    </li>
+                        <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="bi bi-box-seam"></i> 在庫管理</a>
+                    </li>
+                        <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="bi bi-bar-chart-line-fill"></i> 売上管理</a>
+                    </li>
+                        <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="bi bi-cart-check-fill"></i> 発注管理</a>
+                    </li>
                 </ul>
             </nav>
 
