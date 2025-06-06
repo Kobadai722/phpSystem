23a6839ca7a50d86e5c0d218151f58a147f2e765
jinzesatai <melq335@gmail.com>
2025-06-06 14:31:03 +0900

    荒野行動の馬が勃起

--- human/main.php
+++ human/main.php
@@ -6,6 +6,7 @@
         <title>人事管理表</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="human.css">
     </head>
     <?php include '../header.php'; ?>
     <body>
@@ -20,7 +21,7 @@
 
 <!-- 編集者ページの切り替え 後々CSSで右寄せ予定 -->
     <select name="edit">
-        <option value="editer.php">編集者画面に切り替える</option>
+        <option value="editer.php" class=editer>編集者画面に切り替える</option>
         <option value="main.php">一般画面に切り替える</option>
     </select>
 
