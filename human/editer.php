1c298180be89577232bbc91f0bdba1d0ccb0c1bd
jinzesatai <melq335@gmail.com>
2025-06-16 14:42:58 +0900

    まよけちゃ

--- human/editer.php
+++ human/editer.php
@@ -6,10 +6,10 @@
     <title>人事管理表</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="human.css">
 </head>
 <?php include '../header.php'; ?>
-<script src="human.js"></script>
 
 <body>
     <h1>人事管理表-編集者モード</h1>
@@ -28,7 +28,7 @@
     ?>
     <div class="mb-3 p-3 border rounded">
         <form>
-            <div class="row g-3 aligh-items-center">
+            <div class="row g-3 align-items-center">
                 <div class="col-auto">
                     <label for="display_mode_select" class="col-form-label">表示モード：</label>
                 </div>
@@ -52,20 +52,24 @@
                     <label for="name_keyword" class="col-form-label">氏名：</label>
                 </div>
                 <div class="col-auto">
-                    <input type="search" id="name_keyword" name="name_keyword" class="form-control" value="<?= htmlspecialchars($_GET['name_keyword'] ?? '', ENT_QUOTES) ?>">
-                    <span onclick="clearInputField(this)" class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer;" title="クリア">
-                        <i class="fas fa-times-circle text-muted"></i>
-                    </span>
+                    <div class="position-relative">
+                        <input type="text" id="name_keyword" name="name_keyword" class="form-control pe-4" value="<?= htmlspecialchars($_GET['name_keyword'] ?? '', ENT_QUOTES) ?>">
+                        <span onclick="clearInputField(this)" class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer;" title="クリア">
+                            <i class="fas fa-times-circle text-muted"></i>
+                        </span>
+                    </div>
                 </div>
 
                 <div class="col-auto">
                     <label for="id_keyword" class="col-form-label">従業員番号：</label>
                 </div>
                 <div class="col-auto">
-                    <input type="search" id="id_keyword" name="id_keyword" class="form-control" value="<?= htmlspecialchars($_GET['id_keyword'] ?? '', ENT_QUOTES) ?>">
-                    <span onclick="clearInputField(this)" class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer;" title="クリア">
-                        <i class="fas fa-times-circle text-muted"></i>
-                    </span>
+                    <div class="position-relative">
+                        <input type="text" id="id_keyword" name="id_keyword" class="form-control pe-4" value="<?= htmlspecialchars($_GET['id_keyword'] ?? '', ENT_QUOTES) ?>">
+                        <span onclick="clearInputField(this)" class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer;" title="クリア">
+                            <i class="fas fa-times-circle text-muted"></i>
+                        </span>
+                    </div>
                 </div>
 
                 <div class="col-auto">
@@ -157,5 +161,5 @@
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-
+<script src="human.js"></script>
 </html>
