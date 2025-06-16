81d9638f1150b465b69996fb33ab6aabc7c3bb71
tyuando <narinaritosuga@gmail.com>
2025-06-16 22:19:52 +0900

    jsの追加

--- sales/stock_management.php
+++ sales/stock_management.php
@@ -37,4 +37,5 @@
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
 </body>
+<script src="inventory.js"></script>
 </html>
