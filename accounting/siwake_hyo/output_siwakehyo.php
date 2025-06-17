415a7b7036d7d1a85263dac0a39de33e8529a342
esrgdtfdrrthnfytdr <20224033-takahashiryo@hcs.ac.jp>
2025-06-17 14:23:00 +0900

    qawsedrftgyhujikolp;@:[]

--- accounting/siwake_hyo/output_siwakehyo.php
+++ accounting/siwake_hyo/output_siwakehyo.php
@@ -6,7 +6,7 @@
   <title>仕訳帳</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="/phpSystem/accounting/css/siwake.css">
+  <link rel="stylesheet" href="../css/siwake.css">
 </head>
 
 <body>
