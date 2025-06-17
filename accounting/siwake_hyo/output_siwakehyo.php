2c21c3c18469b4d909ff4d7d528abd0db897d0ba
esrgdtfdrrthnfytdr <20224033-takahashiryo@hcs.ac.jp>
2025-06-17 14:50:17 +0900

    wasedrftyuiopl;@

--- accounting/siwake_hyo/output_siwakehyo.php
+++ accounting/siwake_hyo/output_siwakehyo.php
@@ -72,7 +72,7 @@
                         LEFT JOIN ACCOUNTS AS debit_acc ON debit_entry.ACCOUNT_ID = debit_acc.ID
                         LEFT JOIN JOURNAL_ENTRIES AS credit_entry ON h.ID = credit_entry.HEADER_ID AND credit_entry.TYPE = '貸方'
                         LEFT JOIN ACCOUNTS AS credit_acc ON credit_entry.ACCOUNT_ID = credit_acc.ID
-                        ORDER BY h.ENTRY_DATE DESC, h.ID DESC");
+                        ORDER BY h.ID DESC");
             $sql->execute();
             $results = $sql->fetchAll(PDO::FETCH_ASSOC);
 
