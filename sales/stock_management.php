924d4de43056cde31eb9ed3208fb62af107d4836
tyuando <narinaritosuga@gmail.com>
2025-06-17 14:25:01 +0900

    商品のうちか

--- sales/stock_management.php
+++ sales/stock_management.php
@@ -44,7 +44,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                <h5 class="modal-title" id="addConfirmModalLabel">追加の確認</h5>
+                <h5 class="modal-title" id="addConfirmModalLabel">商品の追加</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
