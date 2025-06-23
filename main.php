095d8f96a710feb70122b99917f46486795615b5
tyuando <narinaritosuga@gmail.com>
2025-06-23 14:10:01 +0900

    パスの修正

--- main.php
+++ main.php
@@ -45,7 +45,7 @@ session_start();
             </div>
         </div>
     </div>
-    <p><img src="C:\Users\s20224014\Pictures\ゴードン.webp" alt="ゴードン"></p>
+    <img src="img/ゴードン.webp" alt="ゴードン">
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
