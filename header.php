a25ae304e0f5e98e95712a5a3e10945e8b9efe51
tyuando <narinaritosuga@gmail.com>
2025-06-23 20:00:20 +0900

    headerのリンクを修正

--- header.php
+++ header.php
@@ -17,9 +17,9 @@
                             販売管理
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="/../sales/stock.php">在庫管理システム</a></li>
-                            <li><a class="dropdown-item" href="/../sales/management.php">売上管理システム</a></li>
-                            <li><a class="dropdown-item" href="/../sales/purchase.php">仕入管理システム</a></li>
+                            <li><a class="dropdown-item" href="/../sales/views/stock.php">在庫管理システム</a></li>
+                            <li><a class="dropdown-item" href="/../sales/views/management.php">売上管理システム</a></li>
+                            <li><a class="dropdown-item" href="/../sales/views/purchase.php">仕入管理システム</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
