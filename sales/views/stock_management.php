6cd6f9c1faa09ef8d7b603ef4c5420f44a39e47b
tyuando <narinaritosuga@gmail.com>
2025-07-11 09:33:16 +0900

    検索フォームの追加

--- sales/views/stock_management.php
+++ sales/views/stock_management.php
@@ -17,13 +17,32 @@
         <?php include '../includes/localNavigation.php'; ?>
         
         <section class="content">
-            <div class="d-flex justify-content-end mb-3">
-                <a href="stock-register.php" class="btn btn-primary">
-                    <i class="bi bi-plus-circle me-2"></i>商品追加
-                </a>
+        <div class="search mt-3">
+            <div class="row g-2 w-100 align-items-center">
+                <div class="col-md-auto position-relative">
+                    <input type="text" id="searchInput" class="form-control pe-5" placeholder="商品名または商品IDで検索" oninput="toggleClearButton()">
+                    <button type="button" class="btn btn-sm btn-outline-secondary position-absolute end-0 top-50 translate-middle-y me-2"
+                    id="clearButton" onclick="clearSearch()" style="display: none;">
+                    <i class="bi bi-x-lg"></i>
+                    </button>
+                </div>
+                <div class="col-md-auto">
+                    <button class="btn btn-primary search-btn" type="button" onclick="search()">
+                        <i class="bi bi-search me-2"></i>検索
+                    </button>
+                </div>
+                <div class="col"></div>
+                <div class="col-md-auto text-end">
+                    </div>
             </div>
-            
-            <div class="table-responsive">
+        </div>
+        <div class="d-flex justify-content-end mb-3">
+            <a href="stock-register.php" class="btn btn-primary">
+                <i class="bi bi-plus-circle me-2"></i>商品追加
+            </a>
+        </div>
+
+        <div class="table-responsive">
                 <table class="table table-border table-hover table-smaller">
                     <thead>
                         <tr>
