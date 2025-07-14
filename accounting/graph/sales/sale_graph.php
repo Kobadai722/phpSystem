2a77675bb0e993d337ed6eed070787c19adaca0d
esrgdtfdrrthnfytdr <20224033-takahashiryo@hcs.ac.jp>
2025-07-14 15:00:05 +0900

    SALEの続きをやろうの会

--- accounting/graph/sales/sale_graph.php
+++ accounting/graph/sales/sale_graph.php
@@ -1,13 +1,14 @@
 <?php
-    $page_title = '売上データ入力';
-    $current_page = 'sales_input';
+    $page_title = 'グラフ（売上データ）';
+    $current_page = 'sales_graph';
     require_once '../a_header.php';
 
     // 売上の科目IDは8
     // 科目IDが8で、借方のものを抽出する
-    //TODO 仕訳明細テーブルから（id、仕訳ヘッダーid、金額）抽出
-        // $sql = $PDO ->preopare()
+    //仕訳明細テーブルから（id、仕訳ヘッダーid、金額）抽出
+        $sql = $PDO ->preopare('SELECT ID, JOURNAL_HEADER_ID, AMOUNT FROM WHERE ACCOUNT_ID = 8 AS S;') 
         // idと仕訳ヘッダーidで仕訳ヘッダーと結合
+
         // 結合した表から（取引日、金額）を抽出
 
     //TODO SALES_ENTRIES（日付、金額）を追加する。
