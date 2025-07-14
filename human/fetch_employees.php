6d0794d0636a97eab867dc7b3134b1a290f294d7
jinzesatai <melq335@gmail.com>
2025-07-14 14:17:20 +0900

    fetch_employees修正

--- human/fetch_employees.php
+++ human/fetch_employees.php
@@ -7,10 +7,9 @@ header('Content-Type: application/json; charset=UTF-8');
 $name_keyword = $_GET['name_keyword'] ?? null;
 $id_keyword = $_GET['id_keyword'] ?? null;
 $division_id = $_GET['division_id'] ?? null;
-$include_deleted = $_GET['include_deleted'] ?? 'false'; // 新しく追加
 
 // ベースとなるSQLクエリ
-$sql_query = "SELECT e.EMPLOYEE_ID, e.NAME, d.DIVISION_NAME, j.JOB_POSITION_NAME, e.JOINING_DATE, e.EMERGENCY_CELL_NUMBER, e.IS_DELETED
+$sql_query = "SELECT e.EMPLOYEE_ID, e.NAME, d.DIVISION_NAME, j.JOB_POSITION_NAME, e.JOINING_DATE, e.EMERGENCY_CELL_NUMBER
             FROM EMPLOYEE e
             LEFT JOIN DIVISION d ON e.DIVISION_ID = d.DIVISION_ID
             LEFT JOIN JOB_POSITION j ON e.JOB_POSITION_ID = j.JOB_POSITION_ID";
@@ -34,11 +33,6 @@ if (!empty($id_keyword)) {
 if (!empty($division_id)) {
     $conditions[] = "e.DIVISION_ID = ?";
     $params[] = $division_id;
-}
-
-// 削除済み従業員の表示/非表示を制御
-if ($include_deleted === 'false') {
-    $conditions[] = "e.IS_DELETED = 0";
 }
 
 // 検索条件が存在する場合、WHERE句をSQLに追加
