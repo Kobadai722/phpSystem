c7c83ee0725e5de61e2b811a1c6a37d830a7e4ed
esrgdtfdrrthnfytdr <20224033-takahashiryo@hcs.ac.jp>
2025-07-18 14:19:15 +0900

    efrgthyjukilo;p:@l;kouiytj6re543tery6ujkhil./jkihugyftr

--- accounting/siwake_hyo/submit_siwake.php
+++ accounting/siwake_hyo/submit_siwake.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../header.php';
+require_once __DIR__ . '../../config.php';
+require_once __DIR__ . '../../header.php';
 require_once '../a_header.php'; 
 // a_header.php で config.php や header.php が読み込まれていることを想定
 
