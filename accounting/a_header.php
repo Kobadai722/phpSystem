0814b8fdc1442988ba81485517683273312fbf3d
esrgdtfdrrthnfytdr <20224033-takahashiryo@hcs.ac.jp>
2025-07-18 15:00:19 +0900

    dxfcgvh

--- accounting/a_header.php
+++ accounting/a_header.php
@@ -26,7 +26,7 @@
 </head>
 <body>
     <?php
-        require_once __DIR__ . '/config.php';
+        require_once __DIR__ . '/../config.php';
         require_once __DIR__ . '/../header.php';
     ?>
 
