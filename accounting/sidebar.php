8c6821af8b0e9c99f62dad85c0ff046ce856c9b3
esrgdtfdrrthnfytdr <20224033-takahashiryo@hcs.ac.jp>
2025-07-22 14:15:26 +0900

    asedfghjihugytfdrfghbjnm,knjbhgftdrgvbnm,。、ｋ

--- accounting/sidebar.php
+++ accounting/sidebar.php
@@ -31,7 +31,7 @@ $current_page = $current_page ?? '';
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link <?php echo ($current_page === 'graph') ? 'active' : ''; ?>" href="#">
+            <a class="nav-link <?php echo ($current_page === 'graph') ? 'active' : ''; ?>" href="/accounting/graph/sale_list.php">
                 <div class="nav-link-content">
                     <i class="bi bi-bar-chart-line-fill"></i>
                     <span class="nav-link-text">グラフ</span>
