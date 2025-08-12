1cf8764ff73d453c475822cc9b0365eb5ca0e6d8
tyuando <narinaritosuga@gmail.com>
2025-08-12 12:18:44 +0900

    header項目の変更

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="/../sales/views/stock.php">在庫管理システム</a></li>
                             <li><a class="dropdown-item" href="/../sales/views/management.php">売上管理システム</a></li>
-                            <li><a class="dropdown-item" href="/../sales/views/purchase.php">仕入管理システム</a></li>
+                            <li><a class="dropdown-item" href="/../sales/views/purchase.php">注文管理システム</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
