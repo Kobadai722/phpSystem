fc7f1199c02cb9c12bc3a3262f52484c19e4ce79
tyuando <narinaritosuga@gmail.com>
2025-10-30 09:18:11 +0900

    ステータスの追加

--- sales/views/purchase.php
+++ sales/views/purchase.php
@@ -36,6 +36,8 @@
                                     <option value="未払い">未払い</option>
                                     <option value="支払い済み">支払い済み</option>
                                     <option value="発送済み">発送済み</option>
+                                    <option value="注文済み">注文済み</option>
+                                    
                                 </select>
                             </div>
                             <div class="col-12">
