69dd47aa7308806cf91e2f96bcbfed598d4eed1e
jinzesatai <melq335@gmail.com>
2025-11-17 22:41:28 +0900

    4年たってまたこれか

--- human/attendance_api.php
+++ human/attendance_api.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once __DIR__ . '/../config.php';
+require_once '../config.php';
 header('Content-Type: application/json');
 
 if (!isset($_SESSION['employee_id'])) {
