94485b2fbf4c246e49021ea417a205759cd8969c
jinzesatai <melq335@gmail.com>
2025-11-27 16:37:18 +0900

    デバッグ用エラーメッセージの追加

--- human/application_list.php
+++ human/application_list.php
@@ -41,6 +41,11 @@ $type_map = [
             echo '<div class="alert alert-success">' . htmlspecialchars($_SESSION['success_message']) . '</div>';
             unset($_SESSION['success_message']);
         }
+
+        if (isset($_SESSION['error_message'])) {
+            echo '<div class="alert alert-danger">' . htmlspecialchars($_SESSION['error_message']) . '</div>';
+            unset($_SESSION['error_message']);
+        }
         ?>
 
         <div class="table-responsive">
