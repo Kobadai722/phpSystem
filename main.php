448fd458d89031ea730418aeab316e99178e8ec7
jinzesatai <melq335@gmail.com>
2025-11-30 11:26:28 +0900

    一回天気コメントアウト

--- main.php
+++ main.php
@@ -113,7 +113,8 @@ $employee_name = $_SESSION['employee_name'] ?? "ゲスト";
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
         </script>
-        <script src="weather.js"></script>
+        <!--
+        <script src="weather.js"></script>-->
         <script src="background_changer.js"></script>
 
         <script>
